4a4f57cbade60531998efa94673b05f0e8e722c6
dennielestira <estiradenniel.cvsu@gmail.com>
2026-05-27 14:22:26 +0800

    add create_admin management command

---
+++ accounts/managements/commands/create_admin.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand
+from accounts.models import CustomUser, AccountType
+
+class Command(BaseCommand):
+    help = 'Create default super admin'
+
+    def handle(self, *args, **kwargs):
+        if not CustomUser.objects.filter(username='admin').exists():
+            CustomUser.objects.create_superuser(
+                username='Chairperson',
+                email='admin@admin.com',
+                password='Adminaccount',
+                account_type=AccountType.CAMPUS_ADMIN
+            )
+            self.stdout.write('Super admin created successfully')
+        else:
+            self.stdout.write('Super admin already exists')
