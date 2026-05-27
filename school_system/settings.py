3a46c89f13db1acf95786728a6c0112f434a7738
dennielestira <estiradenniel.cvsu@gmail.com>
2026-05-27 13:21:57 +0800

    use plain whitenoise storage

--- school_system/settings.py
+++ school_system/settings.py
@@ -82,7 +82,7 @@ MIDDLEWARE = [
     'accounts.middleware.RedirectAnd404Middleware',
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
