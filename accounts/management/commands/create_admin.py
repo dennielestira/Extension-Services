adadc06ec6b1f52a84bd98ef1aaf27ca868a4fa7
dennielestira <estiradenniel.cvsu@gmail.com>
2026-05-27 14:36:21 +0800

    fix create_admin command

--- accounts/management/commands/create_admin.py
+++ accounts/management/commands/create_admin.py
@@ -5,13 +5,18 @@ class Command(BaseCommand):
     help = 'Create default super admin'
 
     def handle(self, *args, **kwargs):
-        if not CustomUser.objects.filter(username='admin').exists():
-            CustomUser.objects.create_superuser(
+        if not CustomUser.objects.filter(username='chairperson').exists():
+            user = CustomUser(
                 username='chairperson',
                 email='admin@admin.com',
-                password='chairperson123',
-                account_type=AccountType.CAMPUS_ADMIN
+                account_type=AccountType.CAMPUS_ADMIN,
+                full_name='Chairperson',
+                contact_number='00000000000',
+                is_superuser=True,
+                is_staff=True,
             )
-            self.stdout.write('Super admin created successfully')
+            user.set_password('chairperson123')
+            user.save()
+            self.stdout.write('Campus admin created successfully')
         else:
-            self.stdout.write('Super admin already exists')
+            self.stdout.write('Campus admin already exists')
