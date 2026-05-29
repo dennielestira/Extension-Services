8c66d61bd2d0ad6a3ed32a7f0044630071703626
dennielestira <152932072+dennielestira@users.noreply.github.com>
2026-05-29 15:32:56 +0800

    Update settings.py

--- school_system/settings.py
+++ school_system/settings.py
@@ -121,8 +121,11 @@ AWS_ACCESS_KEY_ID = os.environ.get('B2_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('B2_APP_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('B2_BUCKET_NAME')
 AWS_S3_ENDPOINT_URL = os.environ.get('B2_ENDPOINT_URL')
-AWS_DEFAULT_ACL = 'public-read'
+AWS_DEFAULT_ACL = None
 AWS_QUERYSTRING_AUTH = False
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
 AWS_S3_FILE_OVERWRITE = False
 
 TEMPLATES = [
